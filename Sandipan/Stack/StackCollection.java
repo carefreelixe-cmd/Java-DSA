ae229e76e149c748a87a6509969f3e6d47c8ad32
sandipan-das-sd <sd901656@gmail.com>
2025-12-19 01:26:44 +0530

    add implementation of StackCollection class

---
+++ Sandipan/Stack/StackCollection.java
@@ -0,0 +1,17 @@
+package Sandipan.Stack;
+
+import java.util.*;
+
+public class StackCollection {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while (!s.isEmpty()) {
+            System.out.print(s.peek());
+            s.pop();
+            
+        }
+    }
+}
