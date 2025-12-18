827184284b820d98cff86a3f980221789b6a5d1c
sandipan-das-sd <sd901656@gmail.com>
2025-12-19 01:14:08 +0530

    add implementation of StackLinkedList class

---
+++ Sandipan/Stack/StackLinkedList.java
@@ -0,0 +1,61 @@
+package Sandipan.Stack;
+import java.util.*;
+public class StackLinkedList {
+    static class Node{
+        int data;
+        Node next;
+        Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+
+
+
+    }
+
+
+    static class Stack{
+        static Node head=null;
+        
+        public static boolean isEmpty(){
+            return head==null;
+        }
+
+        public static void push(int data)
+        {
+        Node nwNode=new Node(data);
+        if(isEmpty())
+        {
+            head=nwNode;
+            return;
+        }
+        nwNode.next=head;
+        head=nwNode;
+
+
+        }
+
+        public static int pop()
+        {
+            if(isEmpty())
+            {
+                return -1;
+            }
+            int top=head.data;
+            head=head.next;
+            return top;
+        }
+
+        public static int peek()
+        {
+           if(isEmpty())
+           {
+            return -1;
+           }
+           return head.data;
+        }
+        
+
+    }
+}
