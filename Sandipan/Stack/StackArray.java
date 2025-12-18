d8a820fc7a5a91d3427ae90cea2d3875ab17ddad
sandipan-das-sd <sd901656@gmail.com>
2025-12-18 23:29:08 +0530

    add implementation of StackArray and StackArraylist classes

---
+++ Sandipan/Stack/StackArray.java
@@ -0,0 +1,15 @@
+// STACK
+// Can be implemented using:
+// - ARRAY
+// - ARRAYLIST
+// - LINKEDLIST
+
+// STACK is a LIFO (Last In First Out) data structure
+
+// When using ARRAY:
+// We have to check if the array is empty or full as it has a fixed size
+// This is why we don't prefer it
+
+// ARRAYLIST:
+// ArrayList has variable size, so we don't have to check if it is full
+// This makes it a better choice for stack implementation
