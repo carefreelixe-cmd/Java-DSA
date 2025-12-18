91f6f5c5fd371a1daec63c8235f314d04e076753
sandipan-das-sd <sd901656@gmail.com>
2025-12-19 01:15:31 +0530

    refactor StackLinkedList class to improve main method structure

--- Sandipan/Stack/StackLinkedList.java
+++ Sandipan/Stack/StackLinkedList.java
@@ -55,7 +55,21 @@ public class StackLinkedList {
            }
            return head.data;
         }
-        
 
+
+    }
+
+
+    public static void main(String[] args) {
+        Stack s=new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while(!s.isEmpty())
+        {
+            System.out.print(s.peek());
+            s.pop();
+            
+        }
     }
 }
