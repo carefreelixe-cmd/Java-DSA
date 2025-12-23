40661c1636e223149542326e4f8b2ba5fd1e0762
sandipan-das-sd <sd901656@gmail.com>
2025-12-23 15:09:03 +0530

    add implementation of BinarySearch and LargestNumber classes with search and largest number logic

--- Sandipan/Array/LargestNumber.java
+++ Sandipan/Array/LargestNumber.java
@@ -1,5 +1,23 @@
 package Sandipan.Array;
-
+import java.util.*;
 public class LargestNumber {
-    
+    public static int LargestNumber(int arr[] )
+    {
+     int largest=Integer.MIN_VALUE;
+     for(int i=0;i<arr.length;i++)
+     {
+        if(arr[i]>largest)
+        {
+            largest=arr[i];
+
+        }
+      
+     }
+       return largest;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={3,5,7,2,8,1};
+        System.out.println("The largest number in the array is: "+LargestNumber(arr));
+    }
 }
--- Sandipan/Array/LiniearSearch.java
+++ Sandipan/Array/LiniearSearch.java
@@ -1,5 +1,31 @@
 package Sandipan.Array;
 
 public class LiniearSearch {
-    
+    public static int LiniearSearch(int arr[],int key)
+
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==key)
+            {
+                System.out.println("Key found at index: ");
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={2,4,6,8,10,12,14};
+        int key=10;
+        int result=LiniearSearch(arr,key);
+        if(result==-1)
+        {
+            System.out.println("Key not found");
+        }
+        else
+        {
+            System.out.println(result);
+        }
+    }
 }
