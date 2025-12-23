4956b41419aa2e1ebdf21d6462dd243902935c9f
sandipan-das-sd <sd901656@gmail.com>
2025-12-23 23:37:46 +0530

    add implementation of Reversearray and SubarrayPrintJava classes with array reversal and subarray printing logic

--- Sandipan/Array/PairsArrayjava.java
+++ Sandipan/Array/PairsArrayjava.java
@@ -1,5 +1,21 @@
 package Sandipan.Array;
 
 public class PairsArrayjava {
-    
+    public static void printPairsArray(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                System.out.print("("+arr[i]+","+arr[j]+") ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[]={2,4,6,8,10};
+        printPairsArray(arr);
+        
+    }
 }
