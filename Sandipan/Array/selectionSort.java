684addaae072d583439cdcd003ab054c66ecee86
sandipan-das-sd <sd901656@gmail.com>
2025-12-28 23:51:03 +0530

    add implementation of selection sort algorithm

---
+++ Sandipan/Array/selectionSort.java
@@ -0,0 +1,32 @@
+package Sandipan.Array;
+
+public class selectionSort {
+    public static void selectionSort(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            int minIdx=i;
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[j]<arr[minIdx])
+                {
+                    minIdx=j;
+                }
+
+            }
+
+            int temp=arr[minIdx];
+            arr[minIdx]=arr[i];
+            arr[i]=temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[]={5,4,1,3,2};
+        selectionSort(arr);
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
