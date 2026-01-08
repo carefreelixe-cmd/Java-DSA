483ad907bb7a9db0405ea164fa47f118a026e9c9
Soumadip-cmd <soumadipsantra2004@gmail.com>
2026-01-08 14:17:18 +0530

    Strigs

---
+++ Soumadip/string/CompareString.java
@@ -0,0 +1,12 @@
+package Soumadip.string;
+
+public class CompareString {
+      
+      public static void compare(String str)
+      {
+            
+      }
+      public static void main(String[] args) {
+            
+      }
+}
--- Soumadip/string/PalindromeString.java
+++ Soumadip/string/PalindromeString.java
@@ -1,8 +1,35 @@
 package Soumadip.string;
 
+import java.util.*;
+
 public class PalindromeString {
-      public static void palindrome(String str)
+      
+      public static boolean palindrome(String str)
       {
-            
+            int n=str.length();
+            for(int i=0;i<n/2;i++)
+            {
+                  if(str.charAt(i)!=str.charAt(n-i-1)){
+                        return false;
+                  }
+            }
+            return true;
+
+      }
+
+      public static void main (String args[]){
+            String str=new String();
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter any string : ");
+            str=sc.nextLine();
+            boolean val=palindrome(str);
+            if(val==true)
+            {
+                  System.out.println("'"+str+"' the string is palindromic\n\n");
+            }
+            else
+            {
+                  System.out.println("'"+str+"' the string is not palindromic\n\n");
+            }
       }
 }
