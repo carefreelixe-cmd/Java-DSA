c22916b0affb877238acdd69dad69f20a62acc0e
Soumadip-cmd <soumadipsantra2004@gmail.com>
2026-01-10 00:21:23 +0530

    direction string

--- Soumadip/string/DirectionProblem.java
+++ Soumadip/string/DirectionProblem.java
@@ -1,5 +1,39 @@
 package Soumadip.string;
 
 public class DirectionProblem {
-      
+      public static float shortestPath(String str)
+      {
+            int x=0,y=0;
+            for(int i=0;i<str.length();i++)
+            {
+                  char dir=str.charAt(i);
+                  if(dir=='n'||dir=='N')
+                  {
+                        y++;
+                  }
+                  else if(dir=='s'||dir=='S')
+                  {
+                        y--;
+                  }
+                  else if(dir=='w'||dir=='W')
+                  {
+                        x--;
+                  }
+                  else{
+                        x++;
+                  }
+
+
+            }
+            int x2=x*x;
+            int y2=y*y;
+
+            return (float)Math.sqrt(x2+y2);
+      }
+
+      public static void main(String[] args) {
+            
+            float val=shortestPath("wneenesennn");
+            System.out.println("Shortest path is "+val);
+      }
 }
