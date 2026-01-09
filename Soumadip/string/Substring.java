cbae5890273c2e8349a1209d3af877da01549015
Soumadip-cmd <soumadipsantra2004@gmail.com>
2026-01-10 00:56:09 +0530

    uppercase with string builder

--- Soumadip/string/Substring.java
+++ Soumadip/string/Substring.java
@@ -1,5 +1,18 @@
 package Soumadip.string;
 
 public class Substring {
-      
+      public static void subString(String str, int val1, int val2) {
+            for (int i = val1; i < val2; i++) {
+                  System.out.print(str.charAt(i));
+            }
+      }
+
+      public static void main(String[] args) {
+            String str = "Hello World";
+            String str2 = new String(str.substring(2, 7));
+            System.out.println("The Substring is '" + str2 + "'");
+            subString(str, 2, 7);
+
+      }
+
 }
