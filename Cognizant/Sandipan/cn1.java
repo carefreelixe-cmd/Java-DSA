d0058246c31815f6c2a3d94d1dc67d8e67f6fb60
sandipan-das-sd <sd901656@gmail.com>
2026-01-19 22:17:03 +0530

    Implement main method in cn1.java to swap first and last elements of an array and print the result

--- Cognizant/Sandipan/cn1.java
+++ Cognizant/Sandipan/cn1.java
@@ -1,5 +1,18 @@
 package Cognizant.Sandipan;
 
 public class cn1 {
-    
+    public static void main(String[] args) {
+
+        String[] arr = {"Sandipan", "Ranita", "Ankit", "Sourav", "Arpita"};
+
+        if (arr.length > 1) {
+            String temp = arr[0];
+            arr[0] = arr[arr.length - 1];
+            arr[arr.length - 1] = temp;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
