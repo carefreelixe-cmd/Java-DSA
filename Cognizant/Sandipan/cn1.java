412c1ac9461cab5d3b6fb5cb87a506b406f4c2e0
sandipan-das-sd <sd901656@gmail.com>
2026-01-19 22:30:09 +0530

    Refactor cn1.java to include reverseArray method for improved array manipulation

--- Cognizant/Sandipan/cn1.java
+++ Cognizant/Sandipan/cn1.java
@@ -1,7 +1,23 @@
 package Cognizant.Sandipan;
 
 public class cn1 {
+
+    public static void reverseArray(String[] arr) {
+
+    for (int i = 0; i < arr.length / 2; i++) {
+        String temp = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = temp;
+    }
+
+    for (int i = 0; i < arr.length; i++) {
+        System.out.println("This is the reverse array " + arr[i]);
+    }
+}
+
     public static void main(String[] args) {
+
+      
 
         String[] arr = {"Sandipan", "Ranita", "Ankit", "Sourav", "Arpita"};
 
@@ -14,5 +30,10 @@ public class cn1 {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
+
+        reverseArray(arr);
     }
 }
+
+
+
