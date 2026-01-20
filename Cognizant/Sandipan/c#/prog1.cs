ddc71590e7a578544781cb3b64d036f7b00805f4
sandipan-das-sd <sd901656@gmail.com>
2026-01-20 15:04:03 +0530

    Add launch.json for VSCode configuration and create prog1.cs for a basic C# program

---
+++ Cognizant/Sandipan/c#/prog1.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Cognizant.Sandipan.c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
