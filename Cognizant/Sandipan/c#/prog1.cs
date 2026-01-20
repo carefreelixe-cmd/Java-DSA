f588a4ca8fab15fde73fe92ac390657d84ce5c83
sandipan-das-sd <sd901656@gmail.com>
2026-01-20 15:27:27 +0530

    Refactor prog1.cs to improve string initialization and output formatting

--- Cognizant/Sandipan/c#/prog1.cs
+++ Cognizant/Sandipan/c#/prog1.cs
@@ -7,7 +7,9 @@ namespace Cognizant.Sandipan.c_sharp
         {
             int a=10;
             int b;
-
+        String str="Cogziant";
+        String str1="Sandipan";
+        Console.WriteLine("The string is  "+str+" "+str1);
             Console.WriteLine("Hello, World!");
             Console.WriteLine("The string od a is"+a.ToString());
             Console.WriteLine("Enter the value of b:");
