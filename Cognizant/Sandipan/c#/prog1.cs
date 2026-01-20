5b48d6a1ed424c2f0475777cdb8bb1eabc4a27f2
sandipan-das-sd <sd901656@gmail.com>
2026-01-20 15:16:56 +0530

    Add input handling for variable 'b' in prog1.cs

--- Cognizant/Sandipan/c#/prog1.cs
+++ Cognizant/Sandipan/c#/prog1.cs
@@ -5,7 +5,14 @@ namespace Cognizant.Sandipan.c_sharp
     {
         static void Main(string[] args)
         {
+            int a=10;
+            int b;
+
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("The string od a is"+a.ToString());
+            Console.WriteLine("Enter the value of b:");
+            b=Console.ReadLine();
+            Console.WriteLine("The value of b is:"+b);
         }
     }
 }
