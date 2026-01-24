b0a1e1ba9441f70091ea25cc78b5c6742716b6ed
Soumadip-cmd <soumadipsantra2004@gmail.com>
2026-01-25 03:26:46 +0530

    push code c#

--- Cognizant/Soumadip/c#/fib.cs
+++ Cognizant/Soumadip/c#/fib.cs
@@ -1,8 +1,9 @@
-// 0,1,1,2,3,5,8,13,21,.....
-// the logic = 
-/*
-at initially num1=0 , num2=1
-fn=num1+num2;
-num1++ , num2++
-*/
-0 1 35
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello");
+    }
+}
