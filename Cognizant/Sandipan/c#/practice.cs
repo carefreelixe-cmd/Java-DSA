293672fa20026c4662fd270bf22c05b37574420e
sandipan-das-sd <sd901656@gmail.com>
2026-01-26 20:27:13 +0530

    Refactor BinarySearchDemo method visibility and clean up unused namespaces

--- Cognizant/Sandipan/c#/practice.cs
+++ Cognizant/Sandipan/c#/practice.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Numerics;
-using System.Collections.Generic;
+
 class practice
 {
-    public static int BinarySearchDemo(int[]arr,int key)
+   static int BinarySearchDemo(int[]arr,int key)
     {
         int start=0;
         int end=arr.Length-1;
@@ -28,4 +27,38 @@ class practice
         }
         return -1;
     }
+
+    static void MaxSubarray(int []arr)
+    {
+        int cs=0;
+        int max=int.MinValue;
+        for(int i=0;i<arr.Length;i++)
+        {
+            if(cs<0)
+            {
+                cs=0;
+            }
+            cs+=arr[i];
+
+            max=Math.Max(cs,max);
+        }
+    }
+
+    static void Main(string[]args)
+    {
+        int[]arr={10,20,30,40,50,60,70,80,90};
+        Console.WriteLine("Enter the key to search : ");
+        int key=int.Parse(Console.ReadLine());
+        int result=BinarySearchDemo(arr,key);
+        if(result==-1)
+        {
+            Console.WriteLine("Element not found");
+        }
+        else
+        {
+            Console.WriteLine("Element found at index : "+result);
+        }
+    }
+
+
 }
