e414ee05d9912335a54abc3250e646a339cd77b2
Soumadip-cmd <soumadipsantra2004@gmail.com>
2026-02-08 13:38:30 +0530

    using pow

--- cognizent_INT/Armstrong.cs
+++ cognizent_INT/Armstrong.cs
@@ -3,14 +3,14 @@ public class Armstrong
 {
       public static void Main(String[] args)
       {
-            int num=1553;
+            int num=153;
             int temp=num;
             int rem;
             int sum=0;
             while(temp>0)
             {
                   rem=temp%10;
-                  sum+=rem*rem*rem;
+                  sum+=(int)Math.Pow(rem,3);
                   temp/=10;
             }
 
