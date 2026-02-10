b6b760e259cb84d238c5049f42399c5b56b3dcb3
sandipan-das-sd <sd901656@gmail.com>
2026-02-10 18:53:58 +0530

    Refactor reArrangeIncreasingDecresing method for clarity and correct sorting logic

--- tcs/array/reArrangeIncreasingDecresing.java
+++ tcs/array/reArrangeIncreasingDecresing.java
@@ -1,5 +1,27 @@
 package tcs.array;
-
+import java.util.*;
 public class reArrangeIncreasingDecresing {
-    
+    public static void reArrangeIncreasingDecraesing(int []arr)
+
+    {
+        //decresing
+
+        Arrays.sort(arr);
+
+
+        //incressing
+
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]<arr[j])
+                {
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+    }
 }
