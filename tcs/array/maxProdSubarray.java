a493961a53fcf829ce909c19abeda6b873e25908
sandipan-das-sd <sd901656@gmail.com>
2026-02-11 21:04:37 +0530

    Add maxProdSubarray class to calculate maximum subarray sum and symetricPair class to find symmetric pairs in a 2D array

---
+++ tcs/array/maxProdSubarray.java
@@ -0,0 +1,16 @@
+package tcs.array;
+import java.util.*;
+public class maxProdSubarray {
+    public static int  maxSubArraySum(int []arr)
+    {
+     int currentSum=arr[0];
+     int maxSum=arr[0];
+     for(int i=1;i<arr.length;i++)
+     {
+        currentSum=Math.max(arr[i],currentSum+arr[i]);
+        maxSum=Math.max(maxSum,currentSum);
+     }
+
+     return maxSum;
+    }
+}
