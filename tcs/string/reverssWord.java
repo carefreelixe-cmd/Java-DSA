9056b8e2da8f9210d76afba9751ef04f11034dbe
sandipan-das-sd <sd901656@gmail.com>
2026-02-19 11:30:40 +0530

    Refactor reverssWord class to improve word reversal logic and enhance readability

--- tcs/string/reverssWord.java
+++ tcs/string/reverssWord.java
@@ -1,15 +1,30 @@
 package tcs.string;
 
 public class reverssWord {
+
     public static String reverseWord(String s)
     {
-        String [] words=s.split(" ");
-        StringBuilder result=new StringBuilder();
-        for(String word:words)
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++)
         {
-            StringBuilder sb=new StringBuilder(word);
-            result.append(sb.reverse()).append(" ");
+            String word = words[i];
+
+            StringBuilder sb = new StringBuilder(word);
+            result.append(sb.reverse());
+
+            if (i != words.length - 1)   // avoid extra space
+            {
+                result.append(" ");
+            }
         }
-        return s;
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "I love Java";
+        System.out.println(reverseWord(str));
     }
 }
