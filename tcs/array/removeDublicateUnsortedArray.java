8aed855d6aa52b9d7777c91c319e82a7239eff69
sandipan-das-sd <sd901656@gmail.com>
2026-02-23 23:41:18 +0530

    Add method to count unique elements in an unsorted array

--- tcs/array/removeDublicateUnsortedArray.java
+++ tcs/array/removeDublicateUnsortedArray.java
@@ -1,5 +1,25 @@
 package tcs.array;
-
+import java.util.*;
 public class removeDublicateUnsortedArray {
     //same as remove dublicate
+int index=0;
+    public static int CountUniqueElement(int [] arr)
+    {
+       
+        HashSet<Integer>set=new HashSet<Integer>();
+        for(int i= 0;i<arr.length;i++  )
+        {
+            set.add(arr[i]);
+        }
+
+int [] temp=new int [set.size()];
+int index=0;
+int count=0;
+for(Integer val:set)
+{
+    temp[index++]=val;
+    count++;
+}
+return count;
+}
 }
