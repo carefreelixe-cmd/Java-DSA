e238ed6962059129dddfa5540ed46c1213814f6f
Soumadip-cmd <soumadipsantra2004@gmail.com>
2026-05-05 13:42:08 +0530

    the issue

--- cognizent_GENCNEXT/Practice/MyStack/obj/Debug/net10.0/MyStack.AssemblyInfo.cs
+++ cognizent_GENCNEXT/Practice/MyStack/obj/Debug/net10.0/MyStack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyStack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45e0a216e6fa4f24763d68b24a4d220733f5ab4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd058513b34ff390147b219192fcac493639368")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyStack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyStack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
